35b7fbfbaabff96eb063abb501441ebc60998433
sanah13 <Freaks123>
2018-02-08 10:48:29 +0000

    Mapping with 2 activities

--- app/src/main/java/com/example/a3raths25/mapping/MainActivity.java
+++ app/src/main/java/com/example/a3raths25/mapping/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
@@ -19,25 +18,60 @@ import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     MapView mv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Configuration.getInstance().load(this,PreferenceManager.getDefaultSharedPreferences(this));
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_main);
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
-        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
-        Button button =(Button)findViewById(R.id.btn1);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+        Button button = (Button) findViewById(R.id.btn1);
         button.setOnClickListener(this);
     }
-        public void onClick(View view)
-        {
-            EditText lon = (EditText)findViewById(R.id.et2);
-            EditText lat = (EditText)findViewById(R.id.et1);
-            double latci=Double.parseDouble(lat.getText().toString());
-            double lonci= Double.parseDouble(lon.getText().toString());
-            mv.getController().setCenter(new GeoPoint(latci,lonci));
-        }
+
+    public void onClick(View view) {
+        EditText lon = (EditText) findViewById(R.id.et2);
+        EditText lat = (EditText) findViewById(R.id.et1);
+        double latci = Double.parseDouble(lat.getText().toString());
+        double lonci = Double.parseDouble(lon.getText().toString());
+        mv.getController().setCenter(new GeoPoint(latci, lonci));
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        return false;
+    }
+    protected void onActivityResult(int requestCode, int resultCode,Intent intent)
+    {
+            if(requestCode==0)
+            {
+                if (resultCode==RESULT_OK)
+                {
+                    Bundle extras=intent.getExtras();
+                    boolean hikebikemap = extras.getBoolean("com.example.hikebikemap");
+                    if(hikebikemap==true)
+                    {
+                        mv.setTileSource(TileSourceFactory.HIKEBIKEMAP);
+                    }
+                    else
+                    {
+                        mv.setTileSource(TileSourceFactory.MAPNIK);
+                    }
+                }
+            }
+        }
+    }
